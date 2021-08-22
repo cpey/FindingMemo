3950dc81f54e98a93b73f950ce683dd5b11fb5c8
Carles Pey <carles.pey@gmail.com>
2021-08-22 08:58:59 +0400

    Test app arguments

--- test-app/test.c
+++ test-app/test.c
@@ -7,9 +7,12 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
+#include <getopt.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include "test.h"
 
 #define DEVICE 		"tracer"
 #define DEBUGFS		"/sys/kernel/debug"
@@ -24,37 +27,42 @@ typedef struct FUNCTION_NAME {
 #define HOOK_REMOVE  _IOW(HOOK_IOCTL_NUM, 1, FName)
 #define HOOK_INIT    _IOW(HOOK_IOCTL_NUM, 2, FName)
 
-#define _set_exit_err(msg, ...)					\
-	do {										\
-		err=1;									\
-		sprintf(err_msg, msg, ##__VA_ARGS__);	\
-		goto out;								\
-	} while (0)
-
 int main(int argc, char** argv)
 {
 	char device[50];
 	char err_msg[200];
-	int fd;
-	int err;
+	int fd, err, opt;
+	unsigned long address = 0;
+
+	while ((opt = getopt(argc, argv, "a:")) != -1) {
+		switch (opt) {
+			case 'a':
+				address = strtoul(optarg, NULL, 16);
+				break;
+		}
+	}
+
+	if (!address) {
+		_set_exit_err_errno(EINVAL, "Missing hooking address");
+	}
 
 	if (sprintf(device, "%s/%s", DEBUGFS, DEVICE) < 0) {
-		_set_exit_err("Sprintf error\n");
+		_set_exit_err("Sprintf error");
 	}
 
 	fd = open(device, O_WRONLY);
 	if (fd < 0) {
-		_set_exit_err("Error opening the device: %s\n", strerror(errno));
+		_set_exit_err("Error opening the device: %s", strerror(errno));
 	}
 
 	printf("+ Hook init\n");
 	if (ioctl(fd, HOOK_INIT, NULL) < 0) {
-		_set_exit_err("Hook init error: %s\n", strerror(errno));
+		_set_exit_err("Hook init error: %s", strerror(errno));
 	}
 
 	printf("+ Hook install\n");
 	if (ioctl(fd, HOOK_INSTALL, NULL) < 0) {
-		_set_exit_err("Hook install error: %s\n", strerror(errno));
+		_set_exit_err("Hook install error: %s", strerror(errno));
 	}
 
 out:
---
+++ test-app/test.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2021 Carles Pey <cpey@pm.me>
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+#define _set_exit_err(msg, ...)					\
+	do {										\
+		err = 1;								\
+		sprintf(err_msg, msg, ##__VA_ARGS__);	\
+		goto out;								\
+	} while (0)
+
+#define _set_exit_err_errno(_err, msg, ...)		\
+	do {										\
+		err = 1;								\
+		errno = _err;							\
+		sprintf(err_msg, msg, ##__VA_ARGS__);	\
+		goto out;								\
+	} while (0)
+
+#endif
+
