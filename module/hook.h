98739b0d002ea4ef130d3f74b0020c9d1b760c17
Carles Pey <carles.pey@gmail.com>
2021-09-30 19:33:48 +0400

    Adjust tab size

--- module/hook.h
+++ module/hook.h
@@ -20,15 +20,15 @@ struct fm_hook_metadata {
 
 extern struct list_head fm_hooks;
 
-#define for_each_hook(hook, start, end)					\
-	for (hook = start;									\
-	     (unsigned long)hook < (unsigned long)end;		\
+#define for_each_hook(hook, start, end)			\
+	for (hook = start;				\
+	     (unsigned long)hook < (unsigned long)end;	\
 	     hook++)
 
 #define __FMH_STR_ADECL(t, a)	#a
 #define __FMH_STR_TDECL(t, a)	#t
-#define __FMH_STR_TRET (t)		#t
-#define __MAPR(m, t)			m(t)
+#define __FMH_STR_TRET (t)	#t
+#define __MAPR(m, t)		m(t)
 
 #define FM_HOOK_FUNC_DEFINE1(name, ...) FM_HOOK_FUNC_DEFINEx(1, _##name, __VA_ARGS__)
 #define FM_HOOK_FUNC_DEFINE2(name, ...) FM_HOOK_FUNC_DEFINEx(2, _##name, __VA_ARGS__)
@@ -39,44 +39,44 @@ extern struct list_head fm_hooks;
 
 #define FM_HOOK_FUNC_DEFINE_MAXARGS  6
 
-#define FM_HOOK_FUNC_DEFINEx(x, sname, ...)				\
-	__FM_HOOK_FUNC_DEFINEx(x, sname, __VA_ARGS__)		\
-	__FM_HOOK_WRAP_DECLAREx(x, sname, __VA_ARGS__)		\
-	__FM_HOOK_META_DEFINEx(x, sname, __VA_ARGS__)       \
+#define FM_HOOK_FUNC_DEFINEx(x, sname, ...)		\
+	__FM_HOOK_FUNC_DEFINEx(x, sname, __VA_ARGS__)	\
+	__FM_HOOK_WRAP_DECLAREx(x, sname, __VA_ARGS__)	\
+	__FM_HOOK_META_DEFINEx(x, sname, __VA_ARGS__)	\
 	__FM_HOOK_WRAP_DEFINEx(x, sname, __VA_ARGS__)
 
 #define __FM_HOOK_FUNC_DEFINEx(x, name, rtype, ...)	\
 	rtype (*__do_fm_hook##name)(__MAP(x,__SC_DECL,__VA_ARGS__));
 
-#define __FM_HOOK_WRAP_DECLAREx(x, name, rtype, ...) \
+#define __FM_HOOK_WRAP_DECLAREx(x, name, rtype, ...)	\
 	static inline rtype (__do_fm_wrap##name)(__MAP(x,__SC_DECL,__VA_ARGS__));
 
-#define __FM_HOOK_WRAP_DEFINEx(x, name, rtype, ...) \
+#define __FM_HOOK_WRAP_DEFINEx(x, name, rtype, ...) 	\
 	static inline rtype (__do_fm_wrap##name)(__MAP(x,__SC_DECL,__VA_ARGS__))
 
-#define __FM_HOOK_META_DEFINEx(x, sname, srtype, ...) 	\
-    static const char *types##sname[] = {				\
-        __MAP(x,__FMH_STR_TDECL,__VA_ARGS__)			\
-    };													\
-    static const char *args##sname[] = {				\
-        __MAP(x,__FMH_STR_ADECL,__VA_ARGS__)			\
-    };													\
+#define __FM_HOOK_META_DEFINEx(x, sname, srtype, ...) 			\
+	static const char *types##sname[] = {				\
+		__MAP(x,__FMH_STR_TDECL,__VA_ARGS__)			\
+	};								\
+	static const char *args##sname[] = {				\
+		__MAP(x,__FMH_STR_ADECL,__VA_ARGS__)			\
+	};								\
 	static struct fm_hook_metadata __used				\
-	 __fm_hook_meta##sname = {							\
-		.name 	= #sname,								\
-		.func 	= &__do_fm_hook##sname,					\
-		.wrap 	= __do_fm_wrap##sname,					\
-		.rtype	= #srtype, 								\
-		.types 	= x ? types##sname : NULL,				\
-		.args	= x ? args##sname : NULL,				\
-		.list 	= LIST_HEAD_INIT(__fm_hook_meta##sname.list),  \
-	};													\
+	 __fm_hook_meta##sname = {					\
+		.name 	= #sname,					\
+		.func 	= &__do_fm_hook##sname,				\
+		.wrap 	= __do_fm_wrap##sname,				\
+		.rtype	= #srtype, 					\
+		.types 	= x ? types##sname : NULL,			\
+		.args	= x ? args##sname : NULL,			\
+		.list 	= LIST_HEAD_INIT(__fm_hook_meta##sname.list),  	\
+	};								\
 	static struct fm_hook_metadata __used				\
-	__attribute__((section("__fm_hooks_metadata")))		\
+	__attribute__((section("__fm_hooks_metadata")))			\
 	*__p_fm_hook_meta##sname = &__fm_hook_meta##sname;
 
-#define FM_HOOK_FUNC_NAME(name)			__do_fm_hook_##name
-#define FM_HOOK_WRAP_NAME(name)			__do_fm_wrap_##name
+#define FM_HOOK_FUNC_NAME(name)	__do_fm_hook_##name
+#define FM_HOOK_WRAP_NAME(name)	__do_fm_wrap_##name
 
 int hook_install(FName*);
 int hook_remove(FName*);
